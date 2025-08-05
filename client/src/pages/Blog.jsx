6c1b06e0e7e9289995acbc7c6d3fa9802d2f0aca
maninder <manindersolitaire@gmail.com>
2025-08-05 17:24:16 +0530

    Major Project Quick Blog App using MERN 5th Day

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -4,6 +4,7 @@ import { assets, blog_data, comments_data } from '../assets/assets'
 import Navbar from '../components/Navbar'
 import Moment from 'moment'
 import Footer from '../components/Footer'
+import Loader from '../components/Loader'
 
 const Blog = () => {
   const { id } = useParams()
@@ -89,7 +90,7 @@ const Blog = () => {
       </div>
     <Footer/>
     </div>
-  ) : <div>Loading...</div>
+  ) : <Loader/>
 }
 
 export default Blog
