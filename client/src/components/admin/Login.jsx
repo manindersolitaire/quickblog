a0746262b4dfc64a33357e23df3516682e8e2f8e
maninder <manindersolitaire@gmail.com>
2025-08-07 17:31:26 +0530

    Major Project Quick Blog App using MERN 6th Day

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] =  useState('')
+    const [password, setPassword] =  useState('')
 
     const handleSubmit = async() => {
         e.preventDefault()
@@ -16,11 +19,11 @@ const Login = () => {
              <form onSubmit={handleSubmit} className='mt-6 w-full sm:max-w-md text-gray-600'>
                 <div className='flex flex-col'>
                     <label>Email</label>
-                    <input type="email" required placeholder='Your Email Id' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+                    <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" required placeholder='Your Email Id' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
                 </div>
                 <div className='flex flex-col'>
                     <label>Password</label>
-                    <input type="password" required placeholder='Your Password' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+                    <input onChange={(e)=>setPassword(e.target.value)} value={password}type="password" required placeholder='Your Password' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
                 </div>
                 <button type='submit' className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all'>Login</button>
              </form>
